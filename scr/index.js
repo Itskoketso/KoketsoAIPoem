3d5b67f13094b390a5b0043a39c7cf7514e4b69b
Itskoketso <itskoketso@gmail.com>
2024-10-16 12:04:44 +0200

    Linked the AI API to my js to generate poem.

--- scr/index.js
+++ scr/index.js
@@ -1,12 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Poem here",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 10,
     cursor: "",
   });
 }
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "82f2ff36563otbde1564b5ee447a074a";
+  let context = "";
+  let prompt =
+    "You are a Poem expert. Generate a short, sweet poem. Print each sentence on new line. Sign poem with the SheCodes AI";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
